b7031a25d8eefdbd397400b8f91049819c01b88e
Sina Salimirad <SinaSalimirad@proton.me>
2026-07-17 10:06:02 +0330

    chore: implement basic websocket

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,32 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const clients = new Set<WebSocket>();
+
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+
+  .ws("/ws", {
+    open(ws) {
+      clients.add(ws);
+
+      console.log(`Client ${ws.id} connected`);
+    },
+
+    message(_ws, message) {
+      console.log(message);
+
+      for (const client of clients) {
+        client.send(message);
+      }
+    },
+
+    close(ws, _code, _reason) {
+      clients.delete(ws);
+
+      console.log(`Client ${ws.id} disconnected`);
+    },
+  })
+  .listen(4000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
